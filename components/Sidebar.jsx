14171a93325d07885934b955dd9a9b130c895d06
hugosmoreira <hugo.n4@gmail.com>
2022-07-02 17:49:43 -0700

    update sidebar adding phosphor-react styles

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import styles from './Sidebar.module.css'
+import { PencilSimpleLine } from 'phosphor-react'
 
 const Sidebar = () => {
   return (
     <aside className={styles.sidebar}>
         <img className={styles.cover} src='https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50' alt="" />
         <div className={styles.profile}>
+            <img className={styles.avatar} src="https://github.com/hugosmoreira.png" />
             <strong>Junior Malaquias</strong>
             <span>Web Developer</span>
         </div>
         <footer>
-            <a href="#">Editar Seu Perfil</a>
+            <a href="#">
+                <PencilSimpleLine />
+                Editar perfil
+            </a>
         </footer>
     </aside>
   )
